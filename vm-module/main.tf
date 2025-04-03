8f3cffb997f26796da1146f97158fc6503eea16c
venkat <devops.bheemireddy@gmail.com>
2025-04-03 13:21:02 +1100

    vault changes

--- vm-module/main.tf
+++ vm-module/main.tf
@@ -58,7 +58,6 @@ resource "azurerm_network_security_group" "main" {
   }
 }
 
-
 resource "azurerm_network_interface_security_group_association" "main" {
   network_interface_id      = azurerm_network_interface.main.id
   network_security_group_id = azurerm_network_security_group.main.id
