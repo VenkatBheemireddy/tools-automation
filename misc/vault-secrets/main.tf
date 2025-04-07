d6b10a957de52b6b8d4f3dd2ddd7702981516b20
venkat <devops.bheemireddy@gmail.com>
2025-04-07 21:07:29 +1000

    Project Roboshop Ansible with Multi environment

--- misc/vault-secrets/main.tf
+++ misc/vault-secrets/main.tf
@@ -13,10 +13,16 @@ variable "secrets" {
         admin_password = "DevOps@123456"
       }
     }
-    # roboshop-dev = {
-    #   frontend = {}
-    #   catalogue = {}
-    # }
+    roboshop-dev = {
+      frontend = {
+        catalogue_endpoint = "http://catalogue-dev.azdevopsv82.online:8080"
+        user_endpoint = "http://user-dev.azdevopsv82.online:8080"
+        cart_endpoint = "http://cart-dev.azdevopsv82.online:8080"
+        shipping_endpoint = "http://shipping-dev.azdevopsv82.online:8080"
+        payment_endpoint = "http://payment-dev.azdevopsv82.online:8080"
+      }
+      catalogue = {}
+    }
   }
 }
 
