30ac5202ed4acfc9330d5909dd82fa950d28bd10
venkat <devops.bheemireddy@gmail.com>
2025-05-18 10:53:41 +1000

    Latest Changes

--- misc/github-env-create/main.tf
+++ misc/github-env-create/main.tf
@@ -32,14 +32,14 @@ variable "repos" {
 
 resource "github_repository_environment" "dev" {
   count               = length(var.repos)
-  # environment         = "DEV"
+  environment         = "DEV"
   repository          = var.repos[count.index]
   prevent_self_review = false
 }
 
 resource "github_repository_environment" "qa" {
   count               = length(var.repos)
-  # environment         = "QA"
+  environment         = "QA"
   repository          = var.repos[count.index]
   prevent_self_review = false
   reviewers {
@@ -49,7 +49,7 @@ resource "github_repository_environment" "qa" {
 
 resource "github_repository_environment" "uat" {
   count               = length(var.repos)
-  # environment         = "UAT"
+  environment         = "UAT"
   repository          = var.repos[count.index]
   prevent_self_review = false
   reviewers {
@@ -59,7 +59,7 @@ resource "github_repository_environment" "uat" {
 
 resource "github_repository_environment" "prod" {
   count               = length(var.repos)
-  # environment         = "PROD"
+  environment         = "PROD"
   repository          = var.repos[count.index]
   prevent_self_review = false
   reviewers {
